2f92ccffccbba0de119a5002dbedca4a5d5647fc
sametatabasch <sametatabasch@gmail.com>
2017-05-20 12:18:07 +0300

    log satırları silindi

--- mulakat.js
+++ mulakat.js
@@ -3,15 +3,11 @@
  */
 
 (function () {
-    if (location.pathname == "/sinavlar/sorgu/diger/Ss/2017/sozlesmeli_sonuc_fertdty345tr/Ss_No.asp") {
+    if (location.pathname === "/sinavlar/sorgu/diger/Ss/2017/sozlesmeli_sonuc_fertdty345tr/Ss_No.asp") {
 
         var puan = document.getElementsByTagName('table')[2].childNodes[1].childNodes[4].innerText.replace(/[^\d.]/g, '');
         var brans = document.getElementsByTagName('table')[2].childNodes[1].childNodes[8].innerText.split("\n")[1].trim();
         var tcno = document.getElementsByTagName('table')[2].childNodes[1].childNodes[0].innerText.split("\n")[1].trim();
-
-        console.log(puan);
-        console.log(brans);
-        console.log(tcno);
 
         document.getElementsByTagName("body").item(0).innerHTML += '<div id="window" style="position: fixed;left: 50%;top: 50%;width: 600px;margin-left: -300px;background: #fff;border: 1px solid;padding: 3px;box-shadow: 0px 1px 12px 2px #000;">' +
             '<div class="msg" style="text-align: center;font-size: 2em;"></div><div class="clear"></div><button class="yes button" style="margin: 15px auto;display: block;">Listeye Ekle</button>' +
@@ -21,8 +17,8 @@
         /*yes ve no butonları tıklama dinleyicisi ekle*/
         document.querySelector('.yes').addEventListener('click',
             function () {
-                var url= encodeURI('https://botemulakatsiralamasi.gencbilisim.net/ekle.php?p=' + puan + '&t=' + tcno + '&b=' + brans + '');
-                document.querySelector('#window .msg').innerHTML = '<iframe src="'+url+'" width="100%" height="100%">';
+                var url = encodeURI('https://botemulakatsiralamasi.gencbilisim.net/ekle.php?p=' + puan + '&t=' + tcno + '&b=' + brans + '');
+                document.querySelector('#window .msg').innerHTML = '<iframe src="' + url + '" width="100%" height="100%">';
             });
     } else {
         alert("Bu kod sadece 2017 Mülakatları için kullanılır.")
