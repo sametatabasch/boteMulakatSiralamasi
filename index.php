ef688bc9e144e8b262d8ff7b9d4fc3631a17d5b6
sametatabasch <sametatabasch@gmail.com>
2017-05-20 14:38:23 +0300

    Kpss puanı ekleme kısmı ekleniyor.

--- index.php
+++ index.php
@@ -24,10 +24,17 @@
     <meta charset="utf-8"/>
     <!-- Google İzleme kodu -->
     <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
         ga('create', 'UA-75339179-1', 'auto');
         ga('send', 'pageview');
@@ -39,8 +46,43 @@
 <div class="container">
     <div class="panel panel-primary">
         <!-- Default panel contents -->
-        <div class="panel-heading">BÖTE Mülakat Sıralaması</div>
+        <div class="panel-heading">
+            <h3 class="panel-title">BÖTE Mülakat Sıralaması</h3>
+
+        </div>
         <div class="panel-body">
+            <div class="alert alert-info" role="alert">
+                <div class="pull-left">
+                    Lütfen sonucunuzu
+                    <a class="alert-link" href="https://sonuc.osym.gov.tr/Sorgu.aspx?SonucID=4110"> 2016 KPSS sonuç Sayfasından</a>
+                    kopyalayarak olduğu gibi yazın.
+                </div>
+
+                <div class="btn-group pull-right">
+                    <button type="button" class="btn  btn-success pull-right" data-toggle="modal" data-target=".kpssPuanGir">KPSS Puanı ekle</button>
+                </div>
+                <div class="modal fade kpssPuanGir" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+                    <div class="modal-dialog modal-sm" role="document">
+                        <div class="modal-content">
+                            YAPIM AŞAMASINDA
+                            <form>
+                                <div class="form-group">
+                                    <label for="tcno">TC NO:</label>
+                                    <input class="form-control" type="number" maxlength="11" name="tcno" idtcno>
+                                </div>
+                                <div class="form-group">
+                                    <label for="kpss">Kpss Puanı:</label>
+                                    <input class="form-control" type="number" id="kpss" name="kpss">
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control btn btn-default" type="submit" value="Kaydet">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
             <!-- Table -->
             <table id="siralamaListesi" class="table table-responsive table-striped table-bordered dataTable">
                 <thead>
@@ -93,6 +135,6 @@
         </div>
     </div>
 </div>
-
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
