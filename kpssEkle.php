f1792f4242f9a7b7e5a5e2a19658bd84c2c6eaa6
sametatabasch <sametatabasch@gmail.com>
2017-05-20 15:41:41 +0300

    kpssekle deki hata giderildi

--- kpssEkle.php
+++ kpssEkle.php
@@ -8,39 +8,17 @@ error_reporting(E_ALL);
  * Date: 20.08.2016
  * Time: 18:51
  */
-require_once "functions.php"; ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>BÖTE Mülakat Sıralaması</title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+require_once "functions.php";
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-    <meta name="author" content="Samet ATABAŞ">
-    <meta charset="utf-8"/>
-</head>
-<body>
-<?php
 $kpssPuan = floatval(strip_tags(trim(str_replace(',', '.', $_POST['kpss']))));
 $tcno = strval(strip_tags(trim($_POST['tcno'])));
 
-$tcno= md5(sha1($tcno));
+$tcno = md5(sha1($tcno));
 
-if ($puan<100 && $puan>78)
-    kpssPuanıEkle($tcno,$kpssPuan);
-else{
-    $response= array('status'=>'success','msg'=>"Geçersiz veriler");
+if ($kpssPuan < 100 && $kpssPuan > 78)
+    kpssPuanıEkle($tcno, $kpssPuan);
+else {
+    $response = array('status' => 'success', 'msg' => "Geçersiz veriler");
     header('Content-Type: application/json');
     echo json_encode($response);
 }
-?>
-
-<h3 class=""> Kayıt edilen puan = <?= $puan ?></h3>
-<a class="btn btn-default center-block" href="/" target="_blank">Listeyi Gör</a>
-</body>
-</html>
