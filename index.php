ee791c47d155e9bdc77e917e0d34a5b32bb641fb
sametatabasch <sametatabasch@gmail.com>
2017-05-23 15:09:26 +0300

    Mülakat Frekans Grafik modali geniş modal yapıldı

--- index.php
+++ index.php
@@ -183,8 +183,8 @@
 
 <!-- Grafik modal-->
 
-<div class="modal fade mukalatFrekans" tabindex="-1" role="dialog" aria-labelledby="mulakatFrakansModalLabel">
-    <div class="modal-dialog modal-sm" role="document">
+<div class="modal fade  mukalatFrekans" tabindex="-1" role="dialog" aria-labelledby="mulakatFrakansModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
