2fe46989d647e553bbd2e7c30723555980e59a0b
sametatabasch <sametatabasch@gmail.com>
2017-05-23 15:05:12 +0300

    Mülakat Frekans Grafiği eklendi

--- index.php
+++ index.php
@@ -41,6 +41,7 @@
 
     </script>
     <script type="text/javascript" src="js/jquery.inputmask.bundle.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script>
 </head>
 <body>
 <div class="container">
@@ -54,29 +55,35 @@
             <div class="alert alert-info" role="alert">
                 <div class="pull-left">
                     Lütfen sonucunuzu
-                    <a class="alert-link" href="https://sonuc.osym.gov.tr/Sorgu.aspx?SonucID=4110"> 2016 KPSS sonuç Sayfasından</a>
+                    <a class="alert-link" href="https://sonuc.osym.gov.tr/Sorgu.aspx?SonucID=4110"> 2016 KPSS sonuç
+                        Sayfasından</a>
                     kopyalayarak olduğu gibi yazın.
                 </div>
 
                 <div class="btn-group pull-right">
-                    <button type="button" class="btn  btn-success pull-right" data-toggle="modal" data-target=".kpssPuanGir">KPSS Puanı ekle</button>
+                    <button type="button" class="btn  btn-success pull-right" data-toggle="modal"
+                            data-target=".kpssPuanGir">KPSS Puanı ekle
+                    </button>
                 </div>
                 <div class="modal fade kpssPuanGir" tabindex="-1" role="dialog" aria-labelledby="kpssModalLabel">
                     <div class="modal-dialog modal-sm" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                            aria-hidden="true">&times;</span></button>
                                 <h4 class="modal-title" id="kpssModalLabel">Kpss Puanı Ekle</h4>
                             </div>
                             <div class="modal-body" id="kpssPuanGirModalBody">
                                 <form name="kpssPuanGir" id="kpssPuanGir" method="post" action="kpssEkle.php">
                                     <div class="form-group">
                                         <label for="tcno">TC NO:</label>
-                                        <input class="form-control" type="text" data-inputmask="'mask': '9{11}'"  name="tcno" id="tcno" required>
+                                        <input class="form-control" type="text" data-inputmask="'mask': '9{11}'"
+                                               name="tcno" id="tcno" required>
                                     </div>
                                     <div class="form-group">
                                         <label for="kpss">Kpss Puanı:</label>
-                                        <input class="form-control" data-inputmask="'mask': '1{*}9{1,2}.9{1,4}'"  id="kpss" name="kpss" required>
+                                        <input class="form-control" data-inputmask="'mask': '1{*}9{1,2}.9{1,4}'"
+                                               id="kpss" name="kpss" required>
                                     </div>
                                     <div class="form-group">
                                         <input class="form-control btn btn-default" type="submit" value="Kaydet">
@@ -88,11 +95,11 @@
                 </div>
                 <div class="clearfix"></div>
                 <script type="text/javascript">
-                    $('#kpssPuanGir').on('submit',function () {
+                    $('#kpssPuanGir').on('submit', function () {
                         $.ajax({
-                            type   : 'POST',
-                            url    : $(this).attr('action'),
-                            data   : $(this).serializeArray(),
+                            type: 'POST',
+                            url: $(this).attr('action'),
+                            data: $(this).serializeArray(),
                             success: function (returnData) {
                                 $('#kpssPuanGirModalBody').append(
                                     '<div class="alert alert-' + returnData['status'] + ' alert-dismissable fade in">' +
@@ -102,14 +109,22 @@
                                     '</div>'
                                 );
                                 $('#kpssPuanGir')[0].reset();
-                                var z= setInterval(function () {
+                                var z = setInterval(function () {
                                     $('#kpssPuanGirModalBody .alert').alert('close');
-                                },5000);
+                                }, 5000);
                             }
                         });
                         return false;
                     });
                 </script>
+            </div>
+            <div class="well">Mülakat Puanlarının Frekans Grafiğini Görmek için sağdaki butona tıklayın.
+                <div class="btn-group pull-right">
+                    <button type="button" class="btn  btn-info pull-right" data-toggle="modal"
+                            data-target=".mukalatFrekans">Mülakat Puan Frekansları
+                    </button>
+                </div>
+                <div class="clearfix"></div>
             </div>
             <!-- Table -->
             <table id="siralamaListesi" class="table table-responsive table-striped table-bordered dataTable">
@@ -165,9 +180,64 @@
         </div>
     </div>
 </div>
+
+<!-- Grafik modal-->
+
+<div class="modal fade mukalatFrekans" tabindex="-1" role="dialog" aria-labelledby="mulakatFrakansModalLabel">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="mulakatFrakansModalLabel">Mülakat Puanlarının Frekans Grafiği</h4>
+            </div>
+            <div class="modal-body">
+                <?php
+                $labels = "[";
+                $data = "[";
+                foreach ($db->query("SELECT puan,count(*) AS frekans  FROM `liste` GROUP BY puan ORDER BY puan") as $row) {
+                    $labels .= '"' . $row['puan'] . '",';
+                    $data .= $row['frekans'] . ",";
+                }
+                $labels .= "]";
+                $data .= "]";
+                ?>
+                <canvas id="myChart" width="400" height="400"></canvas>
+                <script>
+                    var ctx = document.getElementById("myChart");
+                    var myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: <?= $labels ?>,
+                            datasets: [{
+                                label: '# Frekans Değeri',
+                                data: <?= $data ?>,
+                                backgroundColor: 'rgba(54, 162, 235, 0.4)',
+                                borderColor: 'rgba(0, 38, 255, 1)',
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true
+                                    }
+                                }]
+                            }
+                        }
+                    });
+                </script>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <script src="js/bootstrap.min.js"></script>
 <script type="text/javascript">
-    $(document).ready(function(){
+    $(document).ready(function () {
         $(":input").inputmask();
     });
 </script>
