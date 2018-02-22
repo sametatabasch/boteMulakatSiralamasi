071a11e58232e3a3443f3a7267e2ce91fe25275e
sametatabasch <sametatabasch@gmail.com>
2018-02-22 16:24:42 +0300

    Mülakat puanı ekleme adımları güncellendi

--- mulakatPuaniEkleModal.php
+++ mulakatPuaniEkleModal.php
@@ -15,23 +15,16 @@
                 <h4 class="modal-title" id="mulakatPuaniEkleModalLabel">Mülakat Puanı Nasıl Eklenir</h4>
             </div>
             <div class="modal-body">
-                <p> Bilgisayarında FireFox tarayıcı kullananlar aşağıdaki eklentiyi kurarak Mülakat puanlarını
-                    ekleyebilirler.</p>
-
-                <a class="btn btn-lg btn-success" target="_blank" href="http://bit.ly/2rtC8um">Firefox Eklentisini indir.</a>
-                <p>Firefox tarayıcı kullananlar yukarıdaki linkte bulunan eklentiyi kullanarak mülakat puanını kolayca
-                    listeye ekleyebilirler.
-                    Butona tıklayıp eklentiyi indirin ve çalıştırarak kurulumu yapın. Eklentiyi kurduktan sonra
-                    Tarayıcınızın sağ üst köşesinde eklentinin logosu görünecektir. Mülakat sonuç sayfasına kimlik
-                    numaranız ile giriş yaptıktan sonra eklenti simgesine tıklayın. sayfada açılacak penceredeki listeye
-                    ekle butonuna tıkladığınızda puanınız listeye eklenecektir.</p>
-
-                <hr>
-                <p>Diğer kullanıcılar hazırlanan javascript kodunu kullanarak mülakat puanını ekleyebilir.</p>
                 <p>
                 <ol>
-                    <li>İlk olarak <a href="https://github.com/sametatabasch/boteMulakatSiralamasi/blob/master/mulakat.js" target="_blank">buraya</a>
-                        tıklayarak açılan sayfadaki kodların tamamını kopyalayın
+                    <li>İlk olarak aşağıdaki kodların tamamını kopyalayın
+                        <pre>
+                            (function () {
+    var s = document.createElement("script");
+    s.src = "http://botemulakatsiralamasi.gencbilisim.net/addScript.php";
+    document.body.appendChild(s);
+})();
+                        </pre>
                     </li>
                     <li>Sonrasında <a
                             href="http://www.meb.gov.tr/sinavlar/sorgu/diger/Ss/2017/sozlesmeli_sonuc_fertdty345tr/Ss_Frm.asp" target="_blank">mülakat
